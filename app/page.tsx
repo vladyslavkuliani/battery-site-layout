bc81186ebd627de355f4c224f94718ff3fce5646
Vladyslav Kuliani <vladyslavkuliani@vladyslavsmbp2.mynetworksettings.com>
2024-07-01 15:47:52 -0700

    Simple page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main>
-      <h1>Battery Site Estimator</h1>
+    <main className="container mx-auto">
+      <h1 className="my-4">Battery Site Estimator</h1>
+      <div className="flex flex-row my-3 p-3 border">
+        <div className="basis-1/2 border">Megapack options</div>
+        <div className="basis-1/2">
+          <div className="border">Summary</div>
+          <div className="border">Layout</div>
+        </div>
+      </div>
     </main>
   );
 }
